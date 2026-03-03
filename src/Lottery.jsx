276e1de62e0e034c0bc5bcd137a2b3af8af2a033
Hitesh saini <hiteshsaini0277@gmail.com>
2026-03-03 09:27:20 +0530

    seventh day

--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import "./Lottery.css";
-import genTicket from "./helper";
+import {genTicket,sum} from "./helper";
 export default function Lottery() {
     let [ticket, setTicket] = useState(genTicket(3));
+    let isWinning=sum(ticket) === 15;
+    let BuyAticket=()=>{
+        setTicket(genTicket(3));
+    }
     return(
         <div>
             <h1>Lottery Game !!!</h1>
@@ -11,6 +15,8 @@ export default function Lottery() {
                   <span>{ticket[1]}</span>
                     <span>{ticket[2]}</span>
             </div>
+            <button onClick={BuyAticket}>Generate A Ticket</button>
+            <h3>{isWinning && "congratulation!!! you have won... "}</h3>
         </div>
     );
 }
--- src/helper.js
+++ src/helper.js
@@ -3,6 +3,15 @@ let arr=new Array(n);
 for(let i=0;i<n;i++){
     arr[i]=Math.floor(Math.random()*10);
 }
+
 return arr;
 }
-export default genTicket;
+function sum(arr){
+    let sum=0;
+    for(let i=0;i<arr.length;i++){
+        sum+=arr[i];
+    }
+return sum;
+}
+
+export {genTicket,sum};
