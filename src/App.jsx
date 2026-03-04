50094e54ea22324c0f429ad769d3cb2d1df3bca2
Hitesh saini <hiteshsaini0277@gmail.com>
2026-03-04 13:35:40 +0530

    seventh day 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import LudoBoard from './LudoBoard';
 import ToDoList from './ToDoList';
 import Lottery from './Lottery';
 import Ticket from './Ticket';
+import {sum} from "./helper";
 function App() {
+  let WinnCondition=(ticket) =>{
+     return ticket.every((num)=>num===ticket[0]);
+  }
   return (
     <>
     {/* <ToDoList /> */}
-<Lottery n={3} winningSum={15} />
+<Lottery n={3} WinnCondition={WinnCondition} />
 {/* <Ticket ticket={[1,2,3]}/>
 <Ticket ticket={[1,2,23,34,3]}/> */}
 
