23aeeb103e1bf81fdfd9c2edbdd3c7ef10d26e92
Hitesh saini <hiteshsaini0277@gmail.com>
2026-03-04 13:18:44 +0530

    seventh day

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import './App.css'
 import LudoBoard from './LudoBoard';
 import ToDoList from './ToDoList';
 import Lottery from './Lottery';
+import Ticket from './Ticket';
 function App() {
   return (
     <>
     {/* <ToDoList /> */}
-<Lottery />
+<Lottery n={3} winningSum={15} />
+{/* <Ticket ticket={[1,2,3]}/>
+<Ticket ticket={[1,2,23,34,3]}/> */}
+
     </>
   )
 }
